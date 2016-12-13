05c1ffc7007694f5ae650386ac098165bdfacc6e
Robert Fentress <learn@vt.edu>
2016-12-13 16:55:27 -0500

    allow embedded conf object (only multi for now)

--- index-multi.js
+++ index-multi.js
@@ -16,7 +16,7 @@ var toolbarTemplate = require("./templates/toolbar.handlebars");
 require("script!./node_modules/axe-core/axe.js");
 
 let MultiPlugin = require("./plugins/multi");
-let CustomPlugin = require("./plugins/custom");
+//let CustomPlugin = require("./plugins/custom");
 
 // From: Gras Double (http://stackoverflow.com/users/289317/gras-double)
 // Reference: http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
@@ -31,6 +31,12 @@ function getParameterByName(name, url) {
 }
 
 class Toolbar {
+
+    constructor() {
+        this.$toolbar = $(toolbarTemplate());
+        this.setPluginsContainer(this.$toolbar.find(".tota11y-plugins"));
+        //this.$pluginsContainer = this.$toolbar.find(".tota11y-plugins");
+    }
 
     setConf(conf) {
         this.conf = conf;
@@ -49,89 +55,75 @@ class Toolbar {
     }
 
     setPluginsContainer(pluginsContainer) {
-        this.pluginsContainer = pluginsContainer;
+        this.$pluginsContainer = pluginsContainer;
     }
 
     getPluginsContainer() {
-        return this.pluginsContainer;
+        return this.$pluginsContainer;
     }
 
     appendFixture(fixture) {
-        var plugin = new MultiPlugin(fixture);
+        let plugin = new MultiPlugin(fixture);
         this.appendPlugin(plugin);
-        //plugin.appendTo(pluginsContainer);
     }
 
     appendTo($el) {
-        var $toolbar = $(toolbarTemplate());
-        $el.append($toolbar);
+        $el.append(this.$toolbar);
 
-        $toolbar.find(".tota11y-toolbar-toggle").click((e) => {
+        this.$toolbar.find(".tota11y-toolbar-toggle").click((e) => {
             e.preventDefault();
             e.stopPropagation();
-            $toolbar.toggleClass("tota11y-expanded")
+            this.$toolbar.toggleClass("tota11y-expanded")
         });
-
-        // Attach each plugin
-        var $pluginsContainer = $toolbar.find(".tota11y-plugins");
-        this.setPluginsContainer($pluginsContainer);
 
     }
 
     appendPlugin(plugin) {
-        console.log(this.getPluginsContainer());
         plugin.appendTo(this.getPluginsContainer());
+    }
+
+    appendPlugins() {
+        plugins.forEach((plugin) => {
+            // Mount the plugin to the list
+            this.appendPlugin(plugin);
+        });
     }
 }
 
 $(function() {
-    var bar = new Toolbar();
+    let bar = new Toolbar();
 
-    var options = (typeof aXeA11y === 'undefined') ? { multi: false } : aXeA11y;
-    var aXeA11yMulti = getParameterByName('aXeA11yMulti');
+    let options = (typeof aXeA11y === 'undefined') ? { multi: false } : aXeA11y;
+    let aXeA11yMulti = getParameterByName('aXeA11yMulti');
 
+    /* unless multi is specifically set in url or embedded object, it is false
+     * and if url parameter overrides embedded object */
     options.multi = ((aXeA11yMulti === "true") || (options.multi === true));
 
-    var multiIsSet = !(((typeof aXeA11y === 'undefined') || ((aXeA11y.multi !== true) && (aXeA11y.multi !== false))) && ((aXeA11yMulti !== "true") || (aXeA11yMulti !== "false")));
-
-    if (options.multi) {
-        console.log('multi true');
-        if (getParameterByName('aXeA11yConf')) { //conf file path provided
-            $.getJSON(getParameterByName('aXeA11yConf'), (data) => {
-                return data;
-            }).fail((msg) => {
-                console.log("Error loading configuration file: " + msg.responseText);
-            }).then((conf) => {
-                bar.setConf(conf);
-                bar.appendTo($("body"));
-                bar.getConf().forEach((fixture) => bar.appendFixture(fixture));
-            }, function () {
-                console.log("Error with callback");
-            });
-        } else if (typeof options.conf !== "undefined") { //in page object provided
-            console.log("monkey");
-            bar.setConf(options.conf);
+    if (getParameterByName('aXeA11yConf')) { //conf file path provided
+        $.getJSON(getParameterByName('aXeA11yConf'), (data) => {
+            return data;
+        }).fail((msg) => {
+            console.log("Error loading configuration file: " + msg.responseText);
+        }).then((conf) => {
+            bar.setConf(conf);
             bar.appendTo($("body"));
             bar.getConf().forEach((fixture) => bar.appendFixture(fixture));
-        } else {
-            console.log("Must must provide configuration, either as file name or internal JSON object");
-        }
-    } else {
-        console.log('multi false');
+        }, function () {
+            console.log("Error with callback");
+        });
+    } else if (typeof options.conf !== "undefined") { //in page object provided
+        bar.setConf(options.conf);
         bar.appendTo($("body"));
-        if ((aXeA11yMulti === "false") && (typeof aXeA11y === 'undefined')) {
-            var custom = new CustomPlugin();
-            bar.appendPlugin(custom);
-        }
-        /*if (multiIsSet) {
-            console.log('multi is set');
-            var custom = new CustomPlugin();
-            bar.appendPlugin(custom);
-        }*/
+        bar.getConf().forEach((fixture) => bar.appendFixture(fixture));
+    } else {
+        //console.log("Must must provide configuration, either as file name or internal JSON object");
+        bar.appendTo($("body"));
+        return bar.appendPlugins();
     }
-    plugins.forEach((plugin) => {
-        // Mount the plugin to the list
-        bar.appendPlugin(plugin);
-    });
+
+    if (options.multi) {
+        bar.appendPlugins();
+    }
 
 });
